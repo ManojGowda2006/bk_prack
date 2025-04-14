ae7b7cce7d7acb346cf6fe914160645c280e8d9c
ManojGowda2006 <manojgowdan807@gmail.com>
2025-04-14 20:02:15 +0530

    running msg

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ const PORT = 3000
 
 app.use(express.json())
 
+
+app.get("/",(req, res) => {
+    res.send("Server is running!!!");
+})
+
 app.listen(PORT, () => {
     console.log(`server is running on the port: ${PORT}`)
 })
